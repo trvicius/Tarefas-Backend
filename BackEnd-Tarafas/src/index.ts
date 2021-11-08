c75027c0465e4ec7fb086a89432a9fdfefb6d1dd
Vinicius Galvao de Franca <vinicius.gfranca98@gmail.com>
2021-11-08 13:38:27 -0300

    Primeiro commit

---
+++ BackEnd-Tarafas/src/index.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import routes from "./routes";
+import * as cors from 'cors';
+ 
+const app = express()
+createConnection()
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(routes)
+ 
+app.listen(3333, ()=>{
+    console.log("Servidor Backend ON!")
+})
